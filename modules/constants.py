6a02250d986fa75f7ce990262e6a66f4823a3c0b
rotarymars <s.goto2050@gmail.com>
2025-11-20 16:46:28 +0900

    importing libcamera

--- modules/constants.py
+++ modules/constants.py
@@ -1,17 +1,18 @@
+import libcamera
 UART_BAUD_RATE = 9600
 UART_TIMEOUT = 0.2
 MOTOR_MAX_SPEED = 2000
 MOTOR_MIN_SPEED = 1000
 Rescue_Camera_Port = 0
 Rescue_Camera_Controls = {
-    "AfMode": controls.AfModeEnum.Continuous,
-    "AfSpeed": controls.AfSpeedEnum.Fast,
-    "AeFlickerMode": controls.AeFlickerModeEnum.Manual,
+    "AfMode": libcamera.controls.AfModeEnum.Continuous,
+    "AfSpeed": libcamera.controls.AfSpeedEnum.Fast,
+    "AeFlickerMode": libcamera.controls.AeFlickerModeEnum.Manual,
     "AeFlickerPeriod": 10000,
-    "AeMeteringMode": controls.AeMeteringModeEnum.Matrix,
+    "AeMeteringMode": libcamera.controls.AeMeteringModeEnum.Matrix,
     "AwbEnable": True,
-    "AwbMode": controls.AwbModeEnum.Indoor,
-    "HdrMode": controls.HdrModeEnum.Off
+    "AwbMode": libcamera.controls.AwbModeEnum.Indoor,
+    "HdrMode": libcamera.controls.HdrModeEnum.Off
 }
 Rescue_Camera_Size = (4608, 2592)
 Rescue_Camera_Formats = "RGB888"
@@ -20,14 +21,14 @@ Rescue_Camera_precallback = None
 
 Linetrace_Camera_Port = 1
 Linetrace_Camera_Controls = {
-    "AfMode": controls.AfModeEnum.Manual,
+    "AfMode": libcamera.controls.AfModeEnum.Manual,
     "LensPosition": 1.0 / 0.03,
-    "AeFlickerMode": controls.AeFlickerModeEnum.Manual,
+    "AeFlickerMode": libcamera.controls.AeFlickerModeEnum.Manual,
     "AeFlickerPeriod": 10000,
-    "AeMeteringMode": controls.AeMeteringModeEnum.Matrix,
+    "AeMeteringMode": libcamera.controls.AeMeteringModeEnum.Matrix,
     "AwbEnable": False,
-    "AwbMode": controls.AwbModeEnum.Indoor,
-    "HdrMode": controls.HdrModeEnum.Night
+    "AwbMode": libcamera.controls.AwbModeEnum.Indoor,
+    "HdrMode": libcamera.controls.HdrModeEnum.Night
 }
 Linetrace_Camera_Size = (4608, 2592)
 Linetrace_Camera_Formats = "RGB888"

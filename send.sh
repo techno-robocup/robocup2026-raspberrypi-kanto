7b70817dc2c51e2607d51561b767d569f824e49d
rotarymars <s.goto2050@gmail.com>
2025-12-19 17:36:50 +0900

    modifying send.sh to not to delete venv

--- send.sh
+++ send.sh
@@ -1,2 +1,2 @@
-rsync -avzc --delete --exclude .direnv/ --exclude .git/ \
+rsync -avzc --delete --exclude .direnv/ --exclude .git/ --exclude .venv/ \
   . robo@roboberry.local:robocup2026-raspberrypi-kanto

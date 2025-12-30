dcbf3d26e176b0870536d47adb2445056731e088
rotarymars <s.goto2050@gmail.com>
2025-12-30 10:34:14 +0900

    adding () in send_speed

--- main.py
+++ main.py
@@ -739,7 +739,7 @@ if __name__ == "__main__":
           sleep_sec(1, robot.send_speed)
           while robot.linetrace_slope is not None:
             robot.set_speed(1400, 1600)
-            robot.send_speed
+            robot.send_speed()
             if robot.robot_stop:
               logger.info("Robot interrupted during object avoidance")
               break

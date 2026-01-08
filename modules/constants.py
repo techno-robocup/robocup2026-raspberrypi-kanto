891f3add92c2efc537c6cd30a8ff765af62d2cc8
rotarymars <s.goto2050@gmail.com>
2026-01-08 12:15:51 +0900

    Adjust upper_red threshold for improved color detection Understanding that the values are invalid

--- modules/constants.py
+++ modules/constants.py
@@ -107,7 +107,7 @@ lower_green = np.array([20, 100, 90])
 upper_green = np.array([100, 255, 255])
 
 lower_red = np.array([160, 110, 110])
-upper_red = np.array([179, 255, 255])
+upper_red = np.array([200, 255, 255])
 
 if __name__ == "__main__":
   pass
